ec22c2b0b5de691e28fe3426c8ba0810915dc967
Bob Cunningham <bobchm@gmail.com>
2022-07-20 11:35:16 -0400

    Class-based implementation of Monsters Rolodex

--- src/App.js
+++ src/App.js
@@ -40,10 +40,11 @@ class App extends Component {
     
     return (
       <div className="App">
+        <h1 className="app-title">Monster's Rolodex</h1>
 
         <SearchBox 
           onChangeHandler={onSearchChange}
-          className='search-box'
+          className='monsters-search-box'
           placeholder='search monsters'
           />
         <CardList monsters={filteredMonsters} />
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,14 +1,17 @@
 import { Component } from "react";
+import './card-list.styles.css';
+import CardContainer from '../card-container/card-container.component.jsx';
 
 class CardList extends Component {
     render() {
         console.log('render from CardList');
         const { monsters } = this.props;
+
         return (
-            <div>
-                {monsters.map((monster) => {
-                    return (<div key={monster.id}><h1>{monster.name}</h1></div>)
-                })}
+            <div className='card-list'>
+                {monsters.map((monster) => 
+                    <CardContainer monster={monster} />
+                )}
             </div>
         )
     }
